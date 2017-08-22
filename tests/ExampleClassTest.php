5e62b9ce5aef04e50ff4dc362a24e2995b273871
Alexey Kopytko <alexey@kopytko.com>
2017-08-22 09:39:51 +0900

    Upgraded to PHPUnit 6.3

--- tests/ExampleClassTest.php
+++ tests/ExampleClassTest.php
@@ -2,7 +2,9 @@
 
 namespace PHPUnitPrimer;
 
-class ExampleClassTest extends \PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class ExampleClassTest extends TestCase
 {
     public function testValueSquared()
     {
